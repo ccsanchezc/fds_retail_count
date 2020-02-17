6fd15571afd107c4d20a17e877cc512abefb352e
ccsanchezc <regimensur01@gmail.com>
2020-02-17 05:41:37 -0800

    ADD ZONA
    ADD zona field Class for the other table un sql lite

--- lib/models/masterdata.dart
+++ lib/models/masterdata.dart
@@ -21,3 +21,27 @@ class Material{
     phone: json["phone"],
   );
 }
+
+class Zona_Field{
+  int id;
+  String name;
+  String phone;
+
+ Zona_Field ({this.id, this.name, this.phone});
+
+  //To insert the data in the bd, we need to convert it into a Map
+  //Para insertar los datos en la bd, necesitamos convertirlo en un Map
+  Map<String, dynamic> toMap() => {
+    "id": id,
+    "name": name,
+    "phone": phone,
+  };
+
+  //to receive the data we need to pass it from Map to json
+  //para recibir los datos necesitamos pasarlo de Map a json
+  factory Zona_Field.fromMap(Map<String, dynamic> json) => new Zona_Field(
+    id: json["id"],
+    name: json["name"],
+    phone: json["phone"],
+  );
+}
