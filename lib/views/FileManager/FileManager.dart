72443bbf42149853d1bc3cf4ae7c540436ac36a7
ccsanchezc <regimensur01@gmail.com>
2020-04-20 13:11:50 -0500

    ADD new login
    login awesome release

--- lib/views/FileManager/FileManager.dart
+++ lib/views/FileManager/FileManager.dart
@@ -11,7 +11,47 @@ class FileManagerPage extends StatefulWidget {
   FileManagerPageState createState() => FileManagerPageState();
 }
 
-class FileManagerPageState extends State<FileManagerPage> {
+class FileManagerPageState extends State<FileManagerPage>  with SingleTickerProviderStateMixin{
+  static final _usernameController = TextEditingController();
+  static final _passController = TextEditingController();
+
+   AnimationController controller;
+   Animation<double> animation ;
+
+  GlobalKey<FormState> _key = GlobalKey();
+
+  String _correo;
+  String _contrasena;
+  String mensaje = '';
+
+  bool _logueado = false;
+  initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+
+    //    Descomentar las siguientes lineas para generar un efecto de "respiracion"
+    animation.addStatusListener((status) {
+       if (status == AnimationStatus.completed) {
+         controller.reverse();
+       } else if (status == AnimationStatus.dismissed) {
+         controller.forward();
+       }
+     });
+
+    controller.forward();
+  }
+
+  dispose() {
+    // Es importante SIEMPRE realizar el dispose del controller.
+    controller.dispose();
+    super.dispose();
+  }
+
+
   final logo = Hero(
     tag: 'hero',
     child: CircleAvatar(
@@ -20,9 +60,96 @@ class FileManagerPageState extends State<FileManagerPage> {
       child: Image.asset('assets/logo.png'),
     ),
   );
-
-  final email = TextFormField(
+  Widget loginForm() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+           // AnimatedLogo(animation: animation),
+          ],
+        ),
+        Container(
+          width: 300.0, //size.width * .6,
+          child: Form(
+            key: _key,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: _usernameController,
+                  validator: (text) {
+                    if (text.length == 0) {
+                      return "Este campo correo es requerido";
+                    }
+                   //   return "El formato para correo no es correcto";
+                  //  }
+                    return null;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  maxLength: 50,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: 'Ingrese su usuario SAP',
+                    labelText: 'Usuario SAP',
+                    counterText: '',
+                    icon:
+                    Icon(Icons.email, size: 32.0, color: Colors.blue[800]),
+                  ),
+                  onSaved: (text) => _correo = text,
+                ),
+                TextFormField(
+                  validator: (text) {
+                    if (text.length == 0) {
+                      return "Este campo contraseña es requerido";
+                    } else if (text.length <= 5) {
+                      return "Su contraseña debe ser al menos de 5 caracteres";
+                    } //else if (!contRegExp.hasMatch(text)) {
+                    //  return "El formato para contraseña no es correcto";
+                  //  }
+                    return null;
+                  },
+                  controller: _passController,
+                  keyboardType: TextInputType.text,
+                  maxLength: 20,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: 'Ingrese su Contraseña',
+                    labelText: 'Contraseña',
+                    counterText: '',
+                    icon: Icon(Icons.lock, size: 32.0, color: Colors.blue[800]),
+                  ),
+                  onSaved: (text) => _contrasena = text,
+                ),
+                IconButton(
+                  onPressed: () {
+                    if (_key.currentState.validate()) {
+                      _key.currentState.save();
+                      //Aqui se llamaria a su API para hacer el login
+                      setState(() {
+                        _logueado = true;
+                      });
+                      mensaje = 'Gracias \n $_correo \n $_contrasena';
+//                      Una forma correcta de llamar a otra pantalla
+//                      Navigator.of(context).push(HomeScreen.route(mensaje));
+                    }
+                  },
+                  icon: Icon(
+                    Icons.arrow_forward,
+                    size: 42.0,
+                    color: Colors.blue[800],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+ /* final email = TextFormField(
     keyboardType: TextInputType.emailAddress,
+    controller: _usernameController,
     autofocus: false,
     initialValue: 'USERSAP',
     decoration: InputDecoration(
@@ -50,19 +177,13 @@ class FileManagerPageState extends State<FileManagerPage> {
         borderRadius: BorderRadius.circular(24),
       ),
       onPressed: () {
-        print("Loggin!");
+        print("Loggin!" + _usernameController.text);
       },
       padding: EdgeInsets.fromLTRB(20.0, 2.0, 20.0, 10.0),
       color: Colors.lightBlueAccent,
       child: Text('Log In', style: TextStyle(color: Colors.white)),
     ),
-  );
-
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  );*/
 
   @override
   Widget build(BuildContext context) {
@@ -85,11 +206,12 @@ class FileManagerPageState extends State<FileManagerPage> {
         children: <Widget>[
           logo,
           SizedBox(height: 20.0),
-          email,
-          SizedBox(height: 8.0),
-          password,
-          SizedBox(height: 24.0),
-          loginButton,
+          loginForm()
+         // email,
+          //SizedBox(height: 8.0),
+          //password,
+          //SizedBox(height: 24.0),
+         //loginButton,
 
         ],
       ),
@@ -249,5 +371,26 @@ class FileManagerPageState extends State<FileManagerPage> {
           );
         });
     ;
+  }
+}
+class AnimatedLogo extends AnimatedWidget {
+  // Maneja los Tween estáticos debido a que estos no cambian.
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1.0);
+  static final _sizeTween = Tween<double>(begin: 0.0, end: 150.0);
+
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return Opacity(
+      opacity: _opacityTween.evaluate(animation),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        height: _sizeTween.evaluate(animation), // Aumenta la altura
+        width: _sizeTween.evaluate(animation), // Aumenta el ancho
+        child: FlutterLogo(),
+      ),
+    );
   }
 }
